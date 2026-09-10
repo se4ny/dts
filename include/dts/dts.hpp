68f3b4f8796b51951c172995a0d41c8572899cf8
Sean Ryan <seantryan@protonmail.com>
2026-09-10 13:26:23 -0700

    add QUATERNION_MAX constexpr

--- include/dts/dts.hpp
+++ include/dts/dts.hpp
@@ -1,6 +1,10 @@
 #ifndef _DTS_HPP
 #define _DTS_HPP
 
+#include <limits>
+
+#include "dts/types.hpp"
+
 #ifdef DTS_SHARED
 #ifdef _DTS_BUILD
 #define DTS_API __declspec(dllexport)
@@ -10,5 +14,12 @@
 #else
 #define DTS_API
 #endif
+
+namespace dts {
+
+DTS_API constexpr dts::types::i16 QUATERNION_MAX =
+    std::numeric_limits<dts::types::i16>::max();
+
+} // namespace dts
 
 #endif // _DTS_HPP
