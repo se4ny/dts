e5f44d9fb49ac5c1bc5ff1601aa6628c472a7fe4
Sean Ryan <seantryan@protonmail.com>
2026-09-11 08:09:15 -0700

    modified _padding of Object to be unnamed padding

--- include/dts/shape/object.hpp
+++ include/dts/shape/object.hpp
@@ -27,7 +27,9 @@ struct DTS_API Object final {
    i32 start_mesh_index{0};
    i32 node_index{0};
    i32 next_sibling{0};
-   i32 _padding{0};
+
+   // Padding
+   i32 : sizeof(i32);
 };
 static_assert(std::is_trivially_copyable_v<Object>);
 
